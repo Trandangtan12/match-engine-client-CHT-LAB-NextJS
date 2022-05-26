b9a1eee68ffff79204fb9e6e2e1fb4f2101b87e1
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2022-05-26 09:20:33 +0700

    update

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styles from "./tabs.module.css";
-import cx from "classnames";
 import BalancePanel from "./TabPanel/Pannel/BalancePanel";
 import GeneralPanel from "./TabPanel/Pannel/GeneralPanel";
 import TrandingPanel from "./TabPanel/Pannel/TrandingPanel";
@@ -10,87 +9,45 @@ const Tabs = () => {
   const toggleTab = (index: number) => {
     setToggleState(index);
   };
+  const tabList = [
+    { id: 1, name: "General" },
+    { id: 2, name: "Balance" },
+    { id: 3, name: "Tranding" },
+    { id: 4, name: "Market" },
+    { id: 5, name: "Websocket" },
+  ];
   return (
-    <div>
-      {" "}
+    <>
       <div className="my-3">
         <ul className="nav nav-tabs">
-          <li className="nav-item">
-            <a
-              className={toggleState === 1 ? "nav-link active" : "nav-link"}
-              onClick={() => toggleTab(1)}
-              aria-current="page"
-              href="#"
-            >
-              General
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={toggleState === 2 ? "nav-link active" : "nav-link"}
-              onClick={() => toggleTab(2)}
-              href="#"
-            >
-              Balance
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={toggleState === 3 ? "nav-link active" : "nav-link"}
-              onClick={() => toggleTab(3)}
-              href="#"
-            >
-              Tranding
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={toggleState === 4 ? "nav-link active" : "nav-link"}
-              onClick={() => toggleTab(4)}
-              href="#"
-            >
-              Market
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={toggleState === 5 ? "nav-link active" : "nav-link"}
-              onClick={() => toggleTab(5)}
-              href="#"
-            >
-              Websocket
-            </a>
-          </li>
+          {tabList.map((item, index) => (
+            <li className="nav-item" key={index}>
+              <a
+                className={
+                  toggleState === item.id ? "nav-link active" : "nav-link"
+                }
+                onClick={() => toggleTab(item.id)}
+                aria-current="page"
+                href="#"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
-      <div
-        className={
-          toggleState === 1
-            ? cx(styles.panel, styles.activePanel)
-            : styles.panel
-        }
-      >
-        <GeneralPanel />
+      <div className="tab-panel">
+        <div className={toggleState === 1 ? styles.activePanel : styles.panel}>
+          <GeneralPanel />
+        </div>
+        <div className={toggleState === 2 ? styles.activePanel : styles.panel}>
+          <BalancePanel />
+        </div>
+        <div className={toggleState === 3 ? styles.activePanel : styles.panel}>
+          <TrandingPanel />
+        </div>
       </div>
-      <div
-        className={
-          toggleState === 2
-            ? cx(styles.panel, styles.activePanel)
-            : styles.panel
-        }
-      >
-        <BalancePanel />
-      </div>
-      <div
-        className={
-          toggleState === 3
-            ? cx(styles.panel, styles.activePanel)
-            : styles.panel
-        }
-      >
-        <TrandingPanel />
-      </div>
-    </div>
+    </>
   );
 };
 
